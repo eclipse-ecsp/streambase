b7d8f9ba93e20bac5a5d5fdf20e476a5e6a5a3a5
Hussain Badshah <Hussain.Badshah@harman.com>
2025-04-24 12:30:48 +0530

    WI 706016 Fixed test case.

--- src/test/java/org/eclipse/ecsp/analytics/stream/base/utils/HiveMQMqttDispatcherIntegrationTest.java
+++ src/test/java/org/eclipse/ecsp/analytics/stream/base/utils/HiveMQMqttDispatcherIntegrationTest.java
@@ -154,10 +154,10 @@ public class HiveMQMqttDispatcherIntegrationTest {
                 .topicFilter(mqttTopicToSubscribe)
                 .callback((publish) -> {
                     logger.info("Msg received:{} on topic:{}", publish.getPayload().get(), publish.getTopic());
-
                     msgReceived = true;
                     mqttTopic = publish.getTopic().toString();
                 }).send();
+        Thread.sleep(TestConstants.THREAD_SLEEP_TIME_5000);
         mqttDispatcher.dispatch(key, value);
         Thread.sleep(TestConstants.THREAD_SLEEP_TIME_5000);
         RetryUtils.retry(TestConstants.TWENTY, (v) -> {
