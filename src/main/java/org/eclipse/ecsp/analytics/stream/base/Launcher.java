6cc3dcd50a78a6464c7e048046b60bdc2ba9c3d8
Arora, Kaushal <Kaushal.Arora@harman.com>
2025-05-22 17:57:38 +0530

    Add harman package to component scan as a workaround

--- src/main/java/org/eclipse/ecsp/analytics/stream/base/Launcher.java
+++ src/main/java/org/eclipse/ecsp/analytics/stream/base/Launcher.java
@@ -76,7 +76,8 @@ import static org.eclipse.ecsp.analytics.stream.base.utils.Constants.COLON_9100;
  */
 @Configuration
 @ComponentScan(basePackages = { "org.eclipse.ecsp", "com.harman" }, excludeFilters
-        = {@ComponentScan.Filter(type = FilterType.REGEX, pattern = "org.eclipse.ecsp.hashicorp.*") })
+        = {@ComponentScan.Filter(type = FilterType.REGEX,
+        pattern = "org.eclipse.ecsp.hashicorp.*|com.harman.hashicorp.*") })
 @PropertySource("classpath:/application-base.properties")
 @PropertySource(ignoreResourceNotFound = true, value = "classpath:/application.properties")
 @PropertySource(ignoreResourceNotFound = true, value = "classpath:/application-ext.properties")
